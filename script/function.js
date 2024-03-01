4fb3972a4fc6e205d389aa75383a4256e5141cad
rimonali123 <rimonali450@gmail.com>
2024-03-01 21:43:06 +0600

    category  added

--- script/function.js
+++ script/function.js
@@ -1,1 +1,14 @@
-console.log('connected sucessfully')
+
+const categories = async() => {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await res.json();
+    const category = document.getElementById("category-container");
+    data.data.news_category.forEach((item) =>{
+        const div = document.createElement('div');
+        div.innerHTML= `<button class="btn-ghost p-2 rounded-lg">${item.category_name}</button>`;
+        category.appendChild(div)
+        // const allCategory = item.category_name;
+        // category.innerText = allCategory;
+    })
+}
+categories()
